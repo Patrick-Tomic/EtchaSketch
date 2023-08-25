b33df43d08ad58e03bfe037de19509fc698d1571
Patrick-Tomic <pat12tomic@gmail.com>
2023-08-25 03:22:35 -0400

    updating creategrid function

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
-const inputs = document.querySelectorAll('.controls input');
+/*const inputs = document.querySelectorAll('.controls input');
 const eraser = document.querySelector("#eraser")
 const reset = document.querySelector("#reset")
 const base = document.querySelector("#base");
 const container = document.querySelector(".container");
 const sketch = document.querySelector("#sketch")
+const controls = document.querySelectorAll(".controls input");
+const gridSize = document.querySelector("#gridSize");
+const scale = document.querySelector("#scale");
+scale.value= 16;
+
+
+
+
+gridSize.textContent = scale.value;  
+scale.addEventListener("mouseup", (event)=>{
+    scale.value = event.target.value;
+    gridSize.textContent = scale.value;  
+    removeGrid();
+    });
+  
+    
+
 
 let flag = false;
 window.onmouseup = ()=>{
     flag = false;
     }
-    const gridSize = document.querySelector("#gridSize");
-    const scale = document.querySelector("#scale");
-    scale.value = 16;
-    gridSize.textContent = scale.value;
-    scale.addEventListener("mouseup", (event)=>{
-        scale.value = event.target.value;
-        gridSize.textContent = scale.value;  
-        removeGrid();
-        });
-       
+   
         function removeGrid(){
             while(sketch.firstChild){
             sketch.removeChild(sketch.firstChild);
@@ -41,6 +49,7 @@ window.onmouseup = ()=>{
             for(let j = 0;j<row;j++){
                 let row = document.createElement("div");
                 row.id = 'row';
+               
                 
                 column.appendChild(row);
             }
@@ -54,11 +63,11 @@ window.onmouseup = ()=>{
         const grid = document.querySelector(".grid");
         const rows = document.querySelectorAll("#row");
      
-        const size = row.style.width;
+        
         createGrid(scale.value);
        
-    
-        console.log(size);
+        
+        
      
       
         
@@ -77,3 +86,56 @@ window.onmouseup = ()=>{
             return;
         } 
         reset.addEventListener("click",()=>location.reload())
+        */
+       const controls = document.querySelectorAll(".controls input");
+       function handleUpdate(){
+        const suffix = this.dataset.sizing ||"";
+        document.documentElement.style.setProperty(`--${this.name}`,this.value+suffix);
+    }
+    controls.forEach(control =>control.addEventListener("change",handleUpdate));
+        const scaleSize = document.querySelector("#scale-size");
+        const scale =document.querySelector(".scale");
+scale.value = 16;        
+scaleSize.textContent = scale.value;  
+scale.addEventListener("mouseup", (event)=>{
+    scale.value = event.target.value;
+    scaleSize.textContent = scale.value;  
+    
+    });
+   
+                    const container = document.querySelector(".gridContainer");
+let bgColor = '#ffffff';
+    
+
+                    function createGrid(){
+                        removeGrid();
+                        let gridSize = scale.value;
+                        //calculates width of each row
+                        let gridWidth = container.offsetWidth/gridSize;
+                        //Assigns how many items to create and at what width
+                        container.style.gridTemplateColumns = `repeat(${gridSize - 3}, ${gridWidth}px) 1fr 1fr 1fr`;
+                        container.style.gridTemplateRows = `repeat(${gridSize - 3}, ${gridWidth}px) 1fr 1fr 1fr`;
+                        if (gridSize < 4) {
+                        container.style.gridTemplateColumns = `repeat(${gridSize},1fr`;
+                        container.style.gridTemplateRows = `repeat(${gridSize}, 1fr`;
+                        }
+
+                                for(let i = 0;i<gridSize**2;i++){
+                                    const item = document.createElement("div");
+                                    item.style.backgroundColor = bgColor;
+                                    item.classList.add("gridItem");
+                                    container.appendChild(item);
+                                }
+                    }
+    createGrid();
+
+    //resets grid once size scale is changed
+    function removeGrid(){
+        while(container.firstChild){
+        container.removeChild(container.firstChild);
+        }
+    }
+
+  
+
+
